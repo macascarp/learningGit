a0d38ff6443e7f1857205ff62a319f78d18360a4
macascarp <maca.scarp@gmail.com>
2021-02-22 23:05:23 +0000

    Spaces are added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,21 @@
 public class HelloWorld {
-    public status void main (String[] args) {                                
+    public status void main (String[] args) {          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+                              
         System.out.printLn("Hello World")
     }
 }
